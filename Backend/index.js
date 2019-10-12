a52ae18f60224cdb5056793ee95d7c879fc7ccf2
william9518 <yuxuanliu@live.ca>
2019-10-12 00:51:20 -0400

    Add request body to response

--- Backend/index.js
+++ Backend/index.js
@@ -11,15 +11,12 @@ const region = 'us-central1';
 exports.reply = (req, res) => {
   let isValid = true;
 
-  // Only validate that requests came from Twilio when the function has been
-  // deployed to production.
   if (process.env.NODE_ENV === 'production') {
     isValid = twilio.validateExpressRequest(req, config.TWILIO_AUTH_TOKEN, {
       url: `https://${region}-${projectId}.cloudfunctions.net/reply`
     });
   }
 
-  // Halt early if the request was not sent from Twilio
   if (!isValid) {
     res
       .type('text/plain')
@@ -29,13 +26,9 @@ exports.reply = (req, res) => {
     return;
   }
 
-  // Prepare a response to the SMS message
   const response = new MessagingResponse();
+  response.message(req.body.Body);
 
-  // Add text to the response
-  response.message('Hello from Google Cloud Functions!');
-
-  // Send the response
   res
     .status(200)
     .type('text/xml')
