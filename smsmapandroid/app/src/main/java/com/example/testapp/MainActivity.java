ff1a8365c2e6cddd9c339b93f36ae14974148897
matttang7 <mt13@illinois.edu>
2019-10-12 00:54:53 -0500

    made location more precise

--- smsmapandroid/app/src/main/java/com/example/testapp/MainActivity.java
+++ smsmapandroid/app/src/main/java/com/example/testapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.location.Location;
 import android.net.Uri;
@@ -25,6 +26,8 @@ import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
     private FusedLocationProviderClient fusedLocationClient;
+    private String twilioNumber = "3074666606";
+    private static String[] latandlong = new String[2];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         TextView test = findViewById(R.id.test);
         Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
 
+        getLatLong();
         if (cursor.moveToFirst()) { // must check the result to prevent exception
             do {
                 String msgData = "";
@@ -68,8 +72,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 try{
                     SmsManager smgr = SmsManager.getDefault();
-                    smgr.sendTextMessage(txtMobile.getText().toString(),null, txtMessage.getText().toString(),null,null);
+                    String textToSend = latandlong[0] + "/" + latandlong[1] + "/" + txtMessage.getText().toString();
+                    //smgr.sendTextMessage(twilioNumber,null, textToSend,null,null);
+                    //smgr.sendTextMessage(txtMobile.getText().toString(),null, textToSend,null,null);
                     Toast.makeText(MainActivity.this, "SMS Sent Successfully", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, DisplayDirections.class));
                 }
                 catch (Exception e){
                     Toast.makeText(MainActivity.this, "SMS Failed to Send, Please try again", Toast.LENGTH_SHORT).show();
@@ -78,6 +85,10 @@ public class MainActivity extends AppCompatActivity {
         });
         final TextView test2 = findViewById(R.id.test2);
         Button getLocation = findViewById(R.id.getloc);
+
+    }
+
+    protected void getLatLong(){
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         fusedLocationClient.getLastLocation()
                 .addOnSuccessListener(this, new OnSuccessListener<Location>() {
@@ -88,7 +99,9 @@ public class MainActivity extends AppCompatActivity {
                             // Logic to handle location object
                             double latitude = location.getLatitude();
                             double longtitude = location.getLongitude();
-                            System.out.println(Double.toString(latitude) + Double.toString(longtitude));
+                            latandlong[0] = Double.toString(latitude);
+                            latandlong[1] = Double.toString(longtitude);
+                            System.out.println("REACHED LAT" + latandlong[0]);
                         }
                     }
                 });
