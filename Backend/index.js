9ec2f5d5d0f3f9307c1009bd99d8ee07070a5030
william9518 <yuxuanliu@live.ca>
2019-10-12 02:12:55 -0400

    Input parsing + Maps direction API polling + output format

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const googleMaps = require('@google/maps').createClient({
+    key: 'AIzaSyC6uGbj_Wog00v0xUV_UNxwZD-HKmyZgWo',
+    Promise: Promise
+});
+
 const twilio = require('twilio');
 const config = require('./config.json');
 
@@ -9,28 +14,61 @@ const projectId = process.env.GCLOUD_PROJECT;
 const region = 'us-central1';
 
 exports.reply = (req, res) => {
-  let isValid = true;
+    // Validation for Twilio
+    let isValid = true;
+    if (process.env.NODE_ENV === 'production') {
+        isValid = twilio.validateExpressRequest(req, config.TWILIO_AUTH_TOKEN, {
+            url: `https://${region}-${projectId}.cloudfunctions.net/reply`
+        });
+    }
+    if (!isValid) {
+        res
+            .type('text/plain')
+            .status(403)
+            .send('Twilio Request Validation Failed.')
+            .end();
+        return;
+    }
 
-  if (process.env.NODE_ENV === 'production') {
-    isValid = twilio.validateExpressRequest(req, config.TWILIO_AUTH_TOKEN, {
-      url: `https://${region}-${projectId}.cloudfunctions.net/reply`
+    // Parse input
+    let inputSplit = req.body.Body.split('/');
+    let latitude = inputSplit[0];
+    let longitude = inputSplit[1];
+    let address = inputSplit[2];
+
+    console.log('Receiving request. latitude='.concat(latitude, ' longitude=', longitude, 'address=', address));
+
+    // retrieve directions
+    googleMaps.directions({
+        origin: latitude.concat(',', longitude),
+        destination: address,
+        mode: 'walking',
+    }).asPromise().then((mapResp) => {
+        console.log('Received response from Directions API!');
+
+        // Generate full message
+        let fullMsg = '';
+        let len = mapResp.json.routes[0].legs[0].steps.length;
+
+        for (let i = 0; i < len; i++) {
+            fullMsg.concat(mapResp.json.routes[0].legs[0].steps[i].html_instructions);
+            if(i != len - 1) {
+                fullMsg.concat(item, '|');
+            } else {
+                //fullMsg.concat(item, '!');
+            }
+        }
+
+        console.log('Sending response: '.concat(fullMsg));
+
+        const textMsg = new MessagingResponse();
+        textMsg.message(fullMsg);
+
+        res
+            .status(200)
+            .type('text/xml')
+            .end(textMsg.toString());
+    }).catch((err) => {
+        console.log(err);
     });
-  }
-
-  if (!isValid) {
-    res
-      .type('text/plain')
-      .status(403)
-      .send('Twilio Request Validation Failed.')
-      .end();
-    return;
-  }
-
-  const response = new MessagingResponse();
-  response.message(req.body.Body);
-
-  res
-    .status(200)
-    .type('text/xml')
-    .end(response.toString());
 };
