bc08df99357f64260be54f63100432135279b50a
matttang7 <mt13@illinois.edu>
2019-10-12 08:31:42 -0500

    added arrow image next to direction

--- smsmapandroid/app/src/main/java/com/example/testapp/DisplayDirections.java
+++ smsmapandroid/app/src/main/java/com/example/testapp/DisplayDirections.java
@@ -1,16 +1,21 @@
 package com.example.testapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class DisplayDirections extends AppCompatActivity {
     //LinearLayout directions =  findViewById(R.id.directions);
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +25,24 @@ public class DisplayDirections extends AppCompatActivity {
         String[] directionsList = MainActivity.direction.split("`");
         for(int i = 0;i < directionsList.length; i++){
             System.out.println(directionsList[i]);
+            LinearLayout layout = new LinearLayout(DisplayDirections.this);
+            layout.setOrientation(LinearLayout.HORIZONTAL);
             LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            final ImageView imageView = new ImageView(this);
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(160, 160)); // value is in pixels
+            imageView.setImageResource(R.drawable.rightarrow);
+            layout.addView(imageView);
+            
             TextView tv=new TextView(this);
             tv.setLayoutParams(lparams);
             tv.setText(directionsList[i]);
             tv.setTextSize(20);
-            directions.addView(tv);
+            layout.addView(tv);
+
+            directions.addView(layout);
+
         }
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
