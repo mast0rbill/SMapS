920f8eb9e5a77f1c3ee6474ba33e347770468331
william9518 <yuxuanliu@live.ca>
2019-10-12 10:20:20 -0400

    format

--- Backend/index.js
+++ Backend/index.js
@@ -38,16 +38,18 @@ function getOutputMsg(steps) {
 
         if (i == 0)
             resp = instructArr[i] + '`';
-        else if (i == len-1) {
+        else if (i == len - 1) {
             const ind = instructArr[i].indexOf('Destination');
-            resp = resp.concat('In ', distArr[i-1] , ', ',  instructArr[i].substring(0,1).toLowerCase() ,
-            instructArr[i].substring(1, ind), '`'); 
-            resp = resp.concat('In ', distArr[i] , ', ', 'your ', instructArr[i].substring(ind, ind+1).toLowerCase() ,
-            instructArr[i].substring(ind+1), '`');
+            resp = resp.concat('In ', distArr[i - 1], ', ', instructArr[i].substring(0, 1).toLowerCase(),
+                instructArr[i].substring(1, ind), '`');
+            resp = resp.concat('In ', distArr[i], ', ', 'your ', instructArr[i].substring(ind, ind + 1).toLowerCase(),
+                instructArr[i].substring(ind + 1), '`');
+            
+            console.log('concating dest: '.concat('In ', distArr[i], ', ', 'your ', instructArr[i].substring(ind, ind + 1).toLowerCase(), instructArr[i].substring(ind + 1), '`'));
         }
         else
-            resp = resp.concat('In ', distArr[i-1] , ', ', instructArr[i].substring(0,1).toLowerCase() + 
-            instructArr[i].substring(1), '`');
+            resp = resp.concat('In ', distArr[i - 1], ', ', instructArr[i].substring(0, 1).toLowerCase() +
+                instructArr[i].substring(1), '`');
     }
 
     return resp;
